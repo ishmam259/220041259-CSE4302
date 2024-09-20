721699fcfa5fc4f73e12cfc114c0a459da8ecfae
ishmam259 <ishmamtahmid@iut-dhaka.edu>
2024-09-20 10:14:04 +0600

    lab3

--- lab3/220041259_L03T01.cpp
+++ lab3/220041259_L03T01.cpp
@@ -4,18 +4,18 @@ using  namespace std;
 class Temperature{
     private:
     double temperature;
-    double increment;
+    double increment1;
 
     public:
-    void setIncrementStep(double increment1){
-        increment=increment1;
+    void setIncrementStep(double increment2){
+        increment1=increment2;
     }
     double getTemperature(){
         return temperature;
 
     }
     void increment(){
-        temperature+=increment;
+        temperature+=increment1;
     }
     void resetTemperature(){
         temperature=0;
@@ -27,4 +27,6 @@ int main(){
     Temperature t1;
     t1.getTemperature();
     t1.setIncrementStep(5);
+    t1.increment();
+    t1.resetTemperature();
 }
