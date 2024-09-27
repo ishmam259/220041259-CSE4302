63c7195b2d609ecf854e2d871948d4cc7928180a
ishmam259 <ishmamtahmid@iut-dhaka.edu>
2024-09-27 09:51:15 +0600

    lab4

--- lab4/220041259_L04T01.cpp
+++ lab4/220041259_L04T01.cpp
@@ -2,15 +2,27 @@
 using namespace std;
 
 class Product{
-    public:
+    private:
     struct product{
         string productname,id;
         int quantity,price,totalinventoryprice,max;
     };
+    public:
     product p1;
     Product(){
         p1.quantity=0;
         p1.totalinventoryprice=0;
+        p1.productname="";
+        p1.price=0;
+        p1.max=0;
+        p1.id="";
+    }
+    Product(string name,string id,int quantity,int price,int max){
+        p1.quantity=quantity;
+        p1.productname=name;
+        p1.price=price;
+        p1.max=max;
+        p1.id=id;
     }
     void addToInventory(int addedQuantity){
         if(addedQuantity+p1.quantity<=p1.max){
@@ -58,47 +70,37 @@ class Product{
 };
 
 int main(){
-    Product p2,p3,p4;
-    p2.p1.productname="Apple";
-    p2.p1.id="2200";
-    p2.p1.price=10;
-    p2.p1.quantity=30;
-    p2.p1.max=100;
-
-    p2.displayDetails(); 
-    p2.addToInventory(50);
-    p2.isAvailable(); 
-    p2.purchase(40); 
-    p2.updatePrice(5); 
-    p2.displayInventoryValue() ;
+    Product p2[3];
     
-    p2.displayTotalInventoryValue();
+    p2[0]=Product("Apple","1001",20,10,100);
 
-    p2.p1.productname="Lenovo";
-    p2.p1.id="2100";
-    p2.p1.price=20;
-    p2.p1.quantity=40;
-    p2.p1.max=100;
+    p2[0].displayDetails(); 
+    p2[0].addToInventory(50);
+    p2[0].isAvailable(); 
+    p2[0].purchase(40); 
+    p2[0].updatePrice(5); 
+    p2[0].displayInventoryValue();
+    p2[0].displayTotalInventoryValue();
 
-    p2.displayDetails(); 
-    p2.addToInventory(50);
-    p2.isAvailable(); 
-    p2.purchase(40); 
-    p2.updatePrice(5); 
-    p2.displayInventoryValue();
-    p2.displayTotalInventoryValue();
+    p2[1]=Product("Lenovo","1001",20,10,100);
 
-    p2.p1.productname="Samsung";
-    p2.p1.id="2000";
-    p2.p1.price=40;
-    p2.p1.quantity=20;
-    p2.p1.max=100;
+    p2[1].displayDetails(); 
+    p2[1].addToInventory(50);
+    p2[1].isAvailable(); 
+    p2[1].purchase(40); 
+    p2[1].updatePrice(5); 
+    p2[1].displayInventoryValue();
+    p2[1].displayTotalInventoryValue();
 
-    p2.displayDetails(); 
-    p2.addToInventory(50);
-    p2.isAvailable(); 
-    p2.purchase(40); 
-    p2.updatePrice(5); 
-    p2.displayInventoryValue();
-    p2.displayTotalInventoryValue();
+    p2[2]=Product("Samsung","101",20,10,100);
+
+    p2[2].displayDetails(); 
+    p2[2].addToInventory(50);
+    p2[2].isAvailable(); 
+    p2[2].purchase(40); 
+    p2[2].updatePrice(5); 
+    p2[2].displayInventoryValue() ;
+    
+    
+    
 }
---
+++ lab4/t2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class BankAccount{
+
+    private:
+    struct account{
+        string name,number,type;
+        float balance;
+        const float minbalance;
+    };
+
+    public:
+    account a1;
+    BankAccount(string name,string number,string type, int balance ){
+        a1.name
+        a1.number
+        a1.type=
+        a1.name
+
+    }
+
+};
+
+int main(){
+
+}
